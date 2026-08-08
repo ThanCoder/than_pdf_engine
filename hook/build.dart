e9e012d8884a1977ddf8257b76918d31d6262e0c
ThanCoder <thancoder2019@gmail.com>
2026-08-08 23:21:28 +0630

    update

--- hook/build.dart
+++ hook/build.dart
@@ -1,11 +1,10 @@
+// ignore_for_file: avoid_print
+
 import 'dart:io';
 
 import 'package:code_assets/code_assets.dart';
 import 'package:hooks/hooks.dart';
 import 'package:than_pdf_engine/core/util_ext.dart';
-
-import 'android_lib.dart';
-import 'linux_lib.dart';
 
 void main(List<String> args) async {
   await build(args, (input, output) async {
@@ -27,4 +26,68 @@ void main(List<String> args) async {
     }
     // await buildAllPlatfroms(input, output);
   });
+}
+
+Future<void> linuxLib(
+  String libName,
+  BuildInput input,
+  BuildOutputBuilder output,
+) async {
+  final packageName = input.packageName;
+
+  final wrapperZipFile = File(
+    input.packageRoot
+        .toFilePath()
+        .join('native_libs')
+        .join('pdfium-wrapper-linux-x86_64')
+        .join(libName),
+  );
+
+  // ၃။ Wrapper Library ကို Asset ထဲ ထည့်ခြင်း
+  output.assets.code.add(
+    CodeAsset(
+      package: packageName,
+      name: '${packageName}_bindings_generated.dart',
+      linkMode: DynamicLoadingBundled(),
+      file: wrapperZipFile.uri,
+    ),
+  );
+
+  print('Native assets bundled successfully!');
+}
+
+Future<void> androidLib(
+  String libName,
+  BuildInput input,
+  BuildOutputBuilder output,
+) async {
+  final packageName = input.packageName;
+  final targetArchitecture = input.config.code.targetArchitecture;
+
+  final nativeName = switch (targetArchitecture) {
+    .arm => 'pdfium-wrapper-android-arm',
+    .arm64 => 'pdfium-wrapper-android-arm64',
+
+    _ => UnsupportedError('$targetArchitecture'),
+  };
+
+  final wrapperZipFile = File(
+    input.packageRoot
+        .toFilePath()
+        .join('native_libs')
+        .join(nativeName.toString())
+        .join(libName),
+  );
+
+  // ၃။ Wrapper Library ကို Asset ထဲ ထည့်ခြင်း
+  output.assets.code.add(
+    CodeAsset(
+      package: packageName,
+      name: '${packageName}_bindings_generated.dart',
+      linkMode: DynamicLoadingBundled(),
+      file: wrapperZipFile.uri,
+    ),
+  );
+
+  print('Native assets bundled successfully!');
 }
